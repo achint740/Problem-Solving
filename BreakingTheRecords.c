e76c6993565fb72875c56d9664bbd70450e1cad4
Achint <42889869+achint740@users.noreply.github.com>
2019-02-24 08:43:13 +0530

    Add files via upload
    Question Link : https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem

---
+++ BreakingTheRecords.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  int i;
+  int a[n];
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  int min=a[0];
+  int countmin=0;
+  int j,k;
+  int countmax=0;
+  for(j=1;j<n;j++)
+  {
+    if(a[j]<min)
+    {
+      min=a[j];
+      countmin++;
+    }
+  }
+ int max=a[0];
+  for(k=1;k<n;k++)
+  {
+    if(a[k]>max)
+    {
+      max=a[k];
+      countmax++;
+    }
+  }
+  printf("%d %d",countmax,countmin);
+}
+

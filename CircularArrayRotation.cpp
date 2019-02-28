53b5dd598d149d0fde5ee5283fed0af2a0b49b6c
Achint <42889869+achint740@users.noreply.github.com>
2019-02-28 17:28:00 +0530

    Add files via upload
    Question Link : https://www.hackerrank.com/challenges/circular-array-rotation/problem

---
+++ CircularArrayRotation.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,num,k,q,temp,i,j;
+    cin>>n>>k>>q;
+    k%=n;
+    int a[n+k];
+    for(int i=0;i<n;i++)
+      cin>>a[i];
+
+    i=n+k-1;
+    j=n-1;
+    while(i>k-1)
+        a[i--]=a[j--];
+    i=0;
+    j=n;
+    while(i<k)
+     a[i++]=a[j++];
+     //for(i=0;i<n;i++)
+      //cout<<a[i]<<" ";
+    /*while(k--)
+    {
+        temp=a[n-1];
+        for(int j=n-1;j>0;j--)
+          a[j]=a[j-1];
+        a[0]=temp;
+    }*/
+   while(q--)
+    {
+        cin>>num;
+        cout<<a[num]<<endl;
+    }
+return 0;
+}
+

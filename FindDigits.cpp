540cc1fa7152c5e47443ecc36138a95efcb75367
Achint <42889869+achint740@users.noreply.github.com>
2019-03-16 12:46:13 +0530

    Add files via upload
    Problem Link : https://www.hackerrank.com/challenges/find-digits/problem

---
+++ FindDigits.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the findDigits function below.
+int findDigits(int n)
+ {
+  int dig;
+  int count=0;
+  int n1=n;
+  while(n>0)
+  {
+      dig=n%10;
+      if(dig!=0 && n1%dig==0)
+        count++;
+    n/=10;
+  }
+return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = findDigits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
